5ec067bfcbe9cad6653d40c098c272add6fc79f6
LeeJoonBin <dlwnsqls22@gmail.com>
2026-08-29 15:52:40 +0900

    [Feat] 플레이어 파생클래스 생성

---
+++ Unity_들어가기전_마지막복습/Unity_들어가기전_마지막복습/It's_me.cs
@@ -0,0 +1,13 @@
+﻿namespace Unity_들어가기전_마지막복습;
+
+public class It_s_me : Player
+{
+    const int MAX_HP = 250;
+
+    public It_s_me() : base("Dawn", 27, MAX_HP, 40, 1.5f, true, true)
+    {
+    }
+    
+    //함수 재정의
+    
+}
